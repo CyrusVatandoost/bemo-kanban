2c59a29b32788e806faebd55e2f6fdeb1ca52e0d
CyrusVatandoost <cyrus@imagineware.ph>
2023-02-08 15:05:52 +0800

    Add validation to APIController

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Card;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class APIController extends Controller
 {
     public function getAllCards(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'access_token' => 'required',
+            'date' => 'nullable|date',
+            'status' => 'nullable|in:0,1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
         $access_token = $request->access_token;
 
-        if(AccessTokenController::checkToken($access_token) === false) {
+        if (AccessTokenController::checkToken($access_token) === false) {
             return response()->json(['error' => 'Invalid access token.'], 401);
         }
 
